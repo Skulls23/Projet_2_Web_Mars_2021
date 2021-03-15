199b16403d0bfe1af7e3d420438035940f20920a
Dim145 <dimitri.bleach@gmail.com>
2021-03-15 12:02:11 +0100

    Add .env au gitignore

---
+++ src/Entity/Etablissement.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtablissementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtablissementRepository::class)
+ */
+class Etablissement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $uai;
+
+    /**
+     * @ORM\Column(type="string", length=512)
+     */
+    private $appellation_officelle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $denomination;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $secteur;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $latitude;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $longitude;
+
+    /**
+     * @ORM\Column(type="string", length=1024)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $departement;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code_departement;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $commune;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $region;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $academie;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code_postal;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_ouverture;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUai(): ?string
+    {
+        return $this->uai;
+    }
+
+    public function setUai(string $uai): self
+    {
+        $this->uai = $uai;
+
+        return $this;
+    }
+
+    public function getAppellationOfficelle(): ?string
+    {
+        return $this->appellation_officelle;
+    }
+
+    public function setAppellationOfficelle(string $appellation_officelle): self
+    {
+        $this->appellation_officelle = $appellation_officelle;
+
+        return $this;
+    }
+
+    public function getDenomination(): ?string
+    {
+        return $this->denomination;
+    }
+
+    public function setDenomination(string $denomination): self
+    {
+        $this->denomination = $denomination;
+
+        return $this;
+    }
+
+    public function getSecteur(): ?string
+    {
+        return $this->secteur;
+    }
+
+    public function setSecteur(string $secteur): self
+    {
+        $this->secteur = $secteur;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): self
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): self
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getDepartement(): ?string
+    {
+        return $this->departement;
+    }
+
+    public function setDepartement(string $departement): self
+    {
+        $this->departement = $departement;
+
+        return $this;
+    }
+
+    public function getCodeDepartement(): ?int
+    {
+        return $this->code_departement;
+    }
+
+    public function setCodeDepartement(int $code_departement): self
+    {
+        $this->code_departement = $code_departement;
+
+        return $this;
+    }
+
+    public function getCommune(): ?string
+    {
+        return $this->commune;
+    }
+
+    public function setCommune(string $commune): self
+    {
+        $this->commune = $commune;
+
+        return $this;
+    }
+
+    public function getRegion(): ?string
+    {
+        return $this->region;
+    }
+
+    public function setRegion(string $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getAcademie(): ?string
+    {
+        return $this->academie;
+    }
+
+    public function setAcademie(string $academie): self
+    {
+        $this->academie = $academie;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?int
+    {
+        return $this->code_postal;
+    }
+
+    public function setCodePostal(int $code_postal): self
+    {
+        $this->code_postal = $code_postal;
+
+        return $this;
+    }
+
+    public function getDateOuverture(): ?\DateTimeInterface
+    {
+        return $this->date_ouverture;
+    }
+
+    public function setDateOuverture(\DateTimeInterface $date_ouverture): self
+    {
+        $this->date_ouverture = $date_ouverture;
+
+        return $this;
+    }
+}
