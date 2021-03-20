15443a94a6013a565c8d678f540f2b294d99fd4a
Florian Boireau <florian23russie@hotmail.fr>
2021-03-20 16:03:47 +0100

    Vue des etablissement en fonction des pages

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Etablissement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,5 +26,18 @@ class EtablissementController extends AbstractController
 
 
         return new Response('Lecture réalisé sans echec, les retours sont:<br/>' . $sRet);
+    }
+
+    #[Route('/etablissement/vueEtablissement/{numPage}', name: 'vueEtablissement')]
+    public function getVueEtablissement(int $numPage): Response
+    {
+        $entity_manager = $this->getDoctrine()->getManager();
+        $tabEtablissement = $entity_manager->getRepository(Etablissement::class)->findBy(array(), orderBy: array("id" => "ASC"), limit: 500, offset: ($numPage-1)*500);
+        $sRet = "";
+        foreach ($tabEtablissement as $etablissement)
+        {
+            $sRet .= $etablissement->getAppellationOfficelle()."<br>";
+        }
+        return new Response($sRet);
     }
 }
