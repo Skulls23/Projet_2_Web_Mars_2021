094c0cc211758aaa2a04dac2addf53c49d9b3e67
Florian Boireau <florian23russie@hotmail.fr>
2021-03-20 14:12:34 +0100

    EtablissementFixtures.php typages des données (cast)

--- src/DataFixtures/EtablissementFixtures.php
+++ src/DataFixtures/EtablissementFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Etablissement;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -15,10 +16,10 @@ class EtablissementFixtures extends Fixture
 
         $csv = fopen("./data/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv", "r+");
 
+        $i = 0;
         while(! feof($csv))
         {
             $donnees = fgetcsv($csv, 0, ";"); //taille 0 = taille infinie
-            $i=0;
             if($i > 0 && $donnees) //donnees renvoie false si on est a la derniere ligne
             {
                 $etablissement = new Etablissement();
@@ -27,17 +28,23 @@ class EtablissementFixtures extends Fixture
                 $etablissement->setAppellationOfficelle($donnees[1]);
                 $etablissement->setDenomination        ($donnees[2]);
                 $etablissement->setSecteur             ($donnees[4]);
-                $etablissement->setLatitude            ($donnees[14]);
-                $etablissement->setLongitude           ($donnees[15]);
+                $etablissement->setLatitude            ((float)$donnees[14]);
+                $etablissement->setLongitude           ((float)$donnees[15]);
                 $etablissement->setAdresse             ($donnees[5]);
                 $etablissement->setDepartement         ($donnees[26]);
-                $etablissement->setCodeDepartement     ($donnees[22]);
+                $etablissement->setCodeDepartement     ((int)$donnees[22]);
                 $etablissement->setCommune             ($donnees[10]);
                 $etablissement->setRegion              ($donnees[27]);
                 $etablissement->setAcademie            ($donnees[28]);
-                $etablissement->setCodePostal          ($donnees[7]);
-                $etablissement->setDateOuverture       ($donnees[34]);
+                $etablissement->setCodePostal          ((int)$donnees[7]);
+                $etablissement->setDateOuverture       (DateTime::createFromFormat("Y/m/j", $donnees[34]));
+
+                $manager->persist($etablissement);
             }
+
+            if($i % 1000 == 0)
+                $manager->flush();
+            $i++;
         }
 
         $manager->flush();
