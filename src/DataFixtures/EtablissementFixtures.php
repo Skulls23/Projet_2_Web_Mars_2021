235e6620d4f632846644d500d6596a0315a9bd3b
Florian Boireau <florian23russie@hotmail.fr>
2021-03-20 15:22:33 +0100

    EtablissementFixtures.php pleinement fonctionnel, sans overflow

--- src/DataFixtures/EtablissementFixtures.php
+++ src/DataFixtures/EtablissementFixtures.php
@@ -11,10 +11,12 @@ class EtablissementFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
         // $product = new Product();
         // $manager->persist($product);
+        gc_enable();
 
-        $csv = fopen("./data/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv", "r+");
+        $csv = fopen("data/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre.csv", "r");
 
         $i = 0;
         while(! feof($csv))
@@ -28,22 +30,26 @@ class EtablissementFixtures extends Fixture
                 $etablissement->setAppellationOfficelle($donnees[1]);
                 $etablissement->setDenomination        ($donnees[2]);
                 $etablissement->setSecteur             ($donnees[4]);
-                $etablissement->setLatitude            ((float)$donnees[14]);
-                $etablissement->setLongitude           ((float)$donnees[15]);
+                $etablissement->setLatitude            (floatval($donnees[14]));
+                $etablissement->setLongitude           (floatval($donnees[15]));
                 $etablissement->setAdresse             ($donnees[5]);
                 $etablissement->setDepartement         ($donnees[26]);
-                $etablissement->setCodeDepartement     ((int)$donnees[22]);
+                $etablissement->setCodeDepartement     (intval($donnees[22]));
                 $etablissement->setCommune             ($donnees[10]);
                 $etablissement->setRegion              ($donnees[27]);
                 $etablissement->setAcademie            ($donnees[28]);
-                $etablissement->setCodePostal          ((int)$donnees[7]);
-                $etablissement->setDateOuverture       (DateTime::createFromFormat("Y/m/j", $donnees[34]));
+                $etablissement->setCodePostal          (intval($donnees[7]));
+                $etablissement->setDateOuverture       (DateTime::createFromFormat("Y-m-d", $donnees[34]));
 
                 $manager->persist($etablissement);
             }
 
-            if($i % 1000 == 0)
+            if($i % 100 == 0) {
                 $manager->flush();
+                $manager->clear();
+                gc_collect_cycles();
+            }
+
             $i++;
         }
 
