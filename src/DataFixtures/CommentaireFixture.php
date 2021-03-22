808187ae59038841a2f37673459c9ca0b315cfd0
Dim145 <dimitri.bleach@gmail.com>
2021-03-22 12:33:02 +0100

    Commentaire debug

--- src/DataFixtures/CommentaireFixture.php
+++ src/DataFixtures/CommentaireFixture.php
@@ -4,15 +4,17 @@ namespace App\DataFixtures;
 
 use App\Controller\EtablissementController;
 use App\Entity\Commentaire;
+use App\Entity\Etablissement;
 use App\Repository\EtablissementRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class CommentaireFixture extends Fixture
+class CommentaireFixture extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $manager->getConnection()->getConfiguration()->setSQLLogger(null);
+        //$manager->getConnection()->getConfiguration()->setSQLLogger(null);
 
         $commentaire = new Commentaire();
         $commentaire->setAuteur("Niccolò Paganini");
@@ -28,11 +30,17 @@ Mentre la pioggia fuor bagna ben cento
 Caminar Sopra 'l giaccio, e à passo lento
 Per timor di cader gersene intenti;");
         $commentaire->setNote(4);
-        $etablissement = $manager->getRepository(EtablissementRepository::class)->findByUAI("9870033X");
+        $etablissement = $manager->getRepository(Etablissement::class)->findByUAI("0260665G");
         $commentaire->setUai($etablissement);
 
-        $manager->persist();
+        echo "Etablissement: " . ($etablissement == null ? "null" : $etablissement->getId());
 
+        $manager->persist($commentaire);
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(EtablissementFixture::class);
     }
 }
